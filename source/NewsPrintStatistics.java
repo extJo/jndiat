ddd95fafb85adb008f551e65541e29d63c4b4763
wnstner92 <38302822+wnstner92@users.noreply.github.com>
2019-06-06 15:01:37 +0900

    Strategy Pattern
    1. Strategy Pattern 
    2. MyPrinter.class -> NewsPrintStatistics.class(interface),  TitlePrintStatistics.class(interface), PrintNews.java, PrintTitle.java
    3. It is useful when user want to add some print option

---
+++ source/NewsPrintStatistics.java
@@ -0,0 +1,5 @@
+public interface NewsPrintStatistics {
+
+	public void printPlain(String message);
+	public void printColor(String color1, String message, String color2);
+}
---
+++ source/TitlePrintStatistics.java
@@ -0,0 +1,4 @@
+public interface TitlePrintStatistics {
+	public void printColor(String color, String bold, int position, String message, String color2, String bold2);
+	public void printPlain(String message, int position);
+}
