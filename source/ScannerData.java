0ce45ebdd0f17a617c20318edcd396167eb97471
wnstner92 <38302822+wnstner92@users.noreply.github.com>
2019-06-11 22:54:29 +0900

    update to fix conflict

--- source/ScannerData.java
+++ source/ScannerData.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 public class ScannerData implements DataScanStretegy {
 	public List<Integer> openedPorts;
-
+	private PortFactory portFactory;
+	private PortProduct portProduct;
+	
 	public ScannerData() {
 	}
 
@@ -14,41 +16,29 @@ public class ScannerData implements DataScanStretegy {
 	@Override
 	public void scan (Scanner scanner, String ip, String ports, String username, String password){
 		Scanner.myLogger.info("Scanning ports '"+ports+"' of "+ip+" with password '"+username+"' and password '"+password+"'");
-		int nb;
+		int numberOfPorts;
 		boolean connected = false;
 		String[] portsList = new String[] {};
-		if (ports.contains(",")==true){
-			Scanner.myLogger.fine("There is a ',' in ports, split ports to scan each port");
-			portsList = ports.split(",");
-			Scanner.myLogger.fine("Ports to scan:"+Arrays.toString(portsList));
-		}
-		else if (ports.contains("-")==true){
-			Scanner.myLogger.fine("There is a '-' in ports");
-			String[] limits = new String[] {};
-			limits = ports.split("-");
-			int port = 0;
-			List<String> tempPorts = new ArrayList<String>();
-			for( port = Integer.parseInt(limits[0]); port <= Integer.parseInt(limits[1]); port++){
-				tempPorts.add(Integer.toString(port));
-			}
-			portsList = tempPorts.toArray(new String[tempPorts.size()]);
-		}
-		else {
-			//ports contains a port only
-			portsList = new String[] {ports};
-		}
-		for (nb=0; nb<portsList.length; nb++) {
-			Scanner.myLogger.fine("Scanning the port "+portsList[nb]);
-			int portToTest = Integer.valueOf(portsList[nb]);
+		List<String> tempPorts = new ArrayList<String>();
+		portFactory = new PortFactory();
+
+		portProduct = portFactory.create(portsList, tempPorts, ports);
+		portsList = portProduct.makePortList();
+		portProduct.makeLog();
+		
+		for (numberOfPorts=0; numberOfPorts<portsList.length; numberOfPorts++) {
+			Scanner.myLogger.fine("Scanning the port "+portsList[numberOfPorts]);
+			int portToTest = Integer.valueOf(portsList[numberOfPorts]);
 			connected = scanner.connection(ip, portToTest, username, password);
 			if (connected == true){
 				Scanner.myLogger.fine("Target "+ip+":"+portToTest+" : T3 connection establish :)");
 				openedPorts.add(portToTest);
-				scanner.deconnection();
+				scanner.disConnection();
 			}
 			else {
 				Scanner.myLogger.fine("Target "+ip+":"+portToTest+" : T3 connection impossible");
 			}
 		}
 	}
+
 }
